2e6c3b76c7c5599896954eefc2cc1835e88cfd1f
Myra Zhang <zhangjiaqing13@gmail.com>
2020-04-13 01:08:06 +0700

    Update insertform.php

--- insertform.php
+++ insertform.php
@@ -3,22 +3,41 @@
 
       $registration_number = $_POST['registration_number'];
       $oneday_price = $_POST['oneday_price'];
-      $car_status=$_POST['car_status'];
+
       $maintenance_date=$_POST['maintenance_date'];
       $date_work_done=$_POST['date_work_done'];
       $work_done=$_POST['work_done'];
-      $type_id=$_POST['type_id'];
-      $Garage_id=$_POST['Garage_id'];
+
+      if (isset($_POST['type_id']) && is_numeric($_POST['type_id']))
+   $type_id= $_POST['type_id'];
+   else 
+   $type_id= 0;
+
+
+ if (isset($_POST['Garage_id']) && is_numeric($_POST['Garage_id']))
+   $Garage_id= $_POST['Garage_id'];
+else 
+   $Garage_id= 0;
+  
+
+      var_dump($_POST);
 
       require('connect.php');
-      $result = $conn->query("select * from Car");
+      $result = $conn->query("SELECT * from Car");
 
-      $sql = " INSERT INTO Car(registration_number, oneday_price, car_status, maintenance_date, date_work_done, work_done, type_id, Garage_id) values ('$registration_number', '$oneday_price', '$car_status', '$maintenance_date', '$date_work_done','$work_done','$type_id','$Garage_id')";
+
+      $sql = "INSERT INTO Car(registration_number, oneday_price, maintenance_date, date_work_done, work_done, type_id, Garage_id) values ('$registration_number', '$oneday_price', '$maintenance_date', '$date_work_done','$work_done','$type_id','$Garage_id')";
       
       $result = $conn->query($sql);
 
-              header ("Location: index.php");
-//my form adding didn't work, the var dump ($_POST) is showing array 0 
-//I have no idea how to fix it.
+       
+
+      echo "res:";
+      echo $result; 
+      echo $conn-> error; 
+      $conn->close();
+
+              header ("Location: data.php");
+
 
 ?>
