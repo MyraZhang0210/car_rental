aaad0bbf34a31f758997977b91c82d3e0d88bef8
Myra Zhang <zhangjiaqing13@gmail.com>
2020-04-20 06:19:09 +0700

    Create Homepage.php

---
+++ Homepage.php
@@ -0,0 +1,111 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Atomic Age' rel='stylesheet'> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #000000;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: yellow;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #BE2200;
+  color: yellow;
+}
+
+.topnav input[type=text] {
+  float: right;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 16px;
+  border: none;
+  font-size: 17px;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a, .topnav input[type=text] {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+
+h1
+{
+  font-family: 'Atomic Age';
+  font-size: 55px;
+  color:#D60000;;
+}
+
+h3
+{
+  font-family: 'Atomic Age';
+  font-size: 30;
+  color:blue;
+}
+
+</style>
+<title> Cars Rental</title>
+<link rel="shortcut icon" type = "image/png" href="favicon.png">
+</head>
+<body>
+
+<div class="topnav">
+  <div class="container-fluid">
+    <div class="container-header" >
+      <a class="navbar-brand" href="Homepage.php" style="padding-top:5px">
+      <img src="rental.png" alt="Logo" style="width:45px">
+</a>
+</div>
+</div>
+  <a class="active" href="Homepage.php">Home</a>
+  <a href="index.php">Car Details</a>
+  <a href="bookpage.php">Booking Details </a>
+  <a href= "rental.php"> Book Now!!! </a>
+  <a href="https://docs.google.com/document/d/1hzsN-suW5oQsNF2Qf4G9tBy6YbgypinADazERuzflyI/edit?usp=sharing"></i>Help</a></li>
+</div>
+<br>
+<h1 align = "center">  Welcome to Zhang's Family Car Rental </h1>
+<br>
+<br>
+<div class ="container">
+<h3 align = "center" > Zhang's Family Car Rental allows you or your family to have a comfortable trip around the cities when visiting Myanmar. </h3>
+<br>
+<h3 align ="center" class="text-success"> Dear Customer can now book cars on "Book Now!!!".</h3>
+</div>
+</body>
+</html>
