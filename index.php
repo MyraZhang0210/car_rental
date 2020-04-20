cccf26176a1d854069b77723d28a2079c27a1ebc
Myra Zhang <zhangjiaqing13@gmail.com>
2020-04-20 07:28:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <style>
-  table, th, td
-   {
+	table, th, td
+	 {
   
   border: 1px solid white;
   
@@ -43,6 +43,13 @@ p
   font-size: 15px;
 }
 
+form 
+{
+
+  font-family: 'Atomic Age';
+  font-size: 20px;
+
+}
 
 
 * {box-sizing: border-box;}
@@ -109,6 +116,84 @@ body {
 }
 }
 
+.button {
+  background-color: #000000; /* Green */
+  border: none;
+  color: yellow;
+  padding: 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {border-radius: 4px;}
+
+
+
+input[type=text], input[type=number],select, textarea 
+ {
+    width: 50%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+    }
+
+    input[type=submit] {
+    background-color: #D60000;
+    color: yellow;
+    padding: 12px 20px;
+    border: none;
+    cursor: pointer;
+    }
+
+    input[type=submit]:hover {
+    background-color: #FF0000;
+    }
+    .container
+     {
+    border-radius: 5px;
+    background-color: #000000;
+    padding: 10px;
+    }
+    .column
+     {
+    float: left;
+    width: 50%;
+    margin-top: 6px;
+    padding: 20px;
+    }
+
+    .row:after 
+    {
+    content: "";
+    display: table;
+    clear: both;
+    }
+
+    @media screen and (max-width: 700px) 
+    {
+    .row
+     {   
+        flex-direction: column;
+    }
+
+    }
+
+    @media screen and (max-width: 400px) 
+    {
+    .navbar a
+     {
+        float: none;
+        width: 100%;
+    }
+    }
+
+
 </style>
 <title> Cars Rental</title>
 <link rel="shortcut icon" type = "image/png" href="favicon.png">
@@ -121,15 +206,12 @@ body {
 </div>
 </div>
   <a href="Home.php">Home</a>
-   <a class="active" href="data.php">Car Details</a>
+   <a class="active" href="a4.php">Car Details</a>
      <a href="bookpage.php">Booking Details </a>
       <a href="rental.php"> Book Now </a>
       <a href="https://docs.google.com/document/d/1hzsN-suW5oQsNF2Qf4G9tBy6YbgypinADazERuzflyI/edit?usp=sharing"></i>Help</a>
-  <input type="text" placeholder="Search..">
-
   
 </div>
-
 
 
 <h1 class="text-danger"> Cars Details </h1>
@@ -140,59 +222,85 @@ body {
 
 
 
- <?php 
+   <div class="container">
+        <div class="row">
+          <div class="column">
+                <img src="Addnew.png" style="width:100%">
+             </div>
+             <div class="column">
 
-require('connect.php');
-$result = $conn->query("select * from Car");
+             <form action="insertform.php" method="post">
+                <label for="registration_number" style="color:#FFFF00">Registration Number</label>
+                <input type="text" id="registration_number" name="registration_number" placeholder="Registration number..">
+                <br>
+                <label for="oneday_price" style="color:#FFFF00">Price/day</label>
+                <input type="text" id="oneday_price" name="oneday_price" placeholder="Price/day..">
+                <br>
+                <label for="work_done" style="color:#FFFF00">Details Work Done</label>
+                <input type="text" id="work_done" name="work_done" placeholder="Details Work Done">
+                <br>
+                <label for="maintenance_date" style="color:#FFFF00">Maintenance Date</label>
+                <input type="date" id="maintenance_date" name="maintenance_date" >
+                <br>
+                <label for="date_work_done" style="color:#FFFF00">Last Maintenance Date</label>
+                <input type="date" id="date_work_done" name="date_work_done" placeholder="YYYY-MM-DD">
+                 <br>
+                
+              <label for="type_id" style="color:#FFFF00">Type ID</label>
+<?php 
+
+                require('connect.php');
+
+                  $sql= "SELECT * from CarType";
+                  $result = $conn -> query($sql);
+                         echo "<select id=\"type_id\" name = \"type_id\">";
+                  while ($row =$result->fetch_assoc())
+                  {
+             
+                    echo "<option value".$row['type_id'].">".$row['type_id']."</option>";
+                
+          
+                  }
+                      echo "</select>";
+                  $conn->close();
+                  ?>
+
+                
+                     <br>
+                 <label for="Garage_id" style="color:#FFFF00">Garage ID</label>
+
+               <?php 
+              
+                require('connect.php');
+
+                  $sql= "SELECT * from Garage";
+                  $result = $conn -> query($sql);
+                         echo "<select id=\"Garage_id\" name = \"Garage_id\">";
+                  while ($row =$result->fetch_assoc())
+                  {
+             
+                    echo "<option value".$row['Garage_id'].">".$row['Garage_id']."</option>";
+
+          
+                  }
+
+                      echo "</select>";
+                  $conn->close();
+                  ?>
+                
+                     <br>
+      <input type ="submit"= value ="Add ">
+
+            </form>
+            </div>
+        </div>
+        <div class ="container" align="center" >
+  <button class="button button1" onclick="window.location.href = 'a4.php';" >Back to Car Details</button>
+</div>
+</div>
 
 
-$sql = "SELECT * from Car";
-$result = $conn->query($sql);
 
-$return_arr = array();
-
-
-if ($result->num_rows > 0) 
-{
-  echo '<table table-hover>';
-  echo '<tr bgcolor=#1D3F7A>
-  <th>Registeration Number</th>
-  <th>Price/day</th>
-  <th>Next Maintenance Date</th>
-  <th>Maintenane Details</th>
-  <th>Last Maintenance Date</th>
-  <th>Car Type ID </th>
-  <th>Garage Type ID </th>
-
-  
-    </tr>';
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo '<tr bgcolor=#009BD6>';
-      echo '<td>'. $row["registration_number"]. '</td>';
-      echo '<td>'. $row["oneday_price"]. '</td>';
       
-      echo '<td>'. $row["maintenance_date"]. '</td>';
-      echo '<td>'. $row["work_done"]. '</td>';
-      echo '<td>'. $row["date_work_done"]. '</td>';
-      echo '<td>'. $row["type_id"]. '</td>';
-      echo '<td>'. $row["Garage_id"]. '</td>';
-
-
-      
-      echo'</tr>';
-    }
-     echo '</table></br>';
-}
- else {
-    echo "0 results";
-}
-
-
-
-$conn->close();
-?>
-
-
 </body>
 </html>
